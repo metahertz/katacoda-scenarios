9e0069173d3beaa5d901c98d0a4d49eb5d5aa51c
Matt Johnson <matt@bridgecrew.io>
2022-02-24 14:00:27 +0000

    Automation

--- bridgecrew-kubernetes/scripts/intro-fg.sh
+++ bridgecrew-kubernetes/scripts/intro-fg.sh
@@ -1,20 +1,20 @@
 set +x
 
-echo "Waiting for initial workshop setup to complete..."; 
+{ echo "Waiting for initial workshop setup to complete..."; } 2> /dev/null
 
-while [ ! -f /opt/.signals-intro-bg-finished ] ; do tail /opt/.signals-intro-bg-status; sleep 2; done; 
+time while [ ! -f /opt/.signals-intro-bg-finished ] ; do tail /opt/.signals-intro-bg-status 2>/dev/null ; sleep 2; done; 
 
-echo "Bridgecrew Workshop Environment Ready!"
-echo "--------------------------------------"
-echo ""
-echo "KIND Kubernetes cluster info:"
-kubectl cluster-info --context kind-kind
+{ echo "Bridgecrew Workshop Environment Ready!" } 2> /dev/null
+{ echo "--------------------------------------" } 2> /dev/null
+{ echo "" } 2> /dev/null
+{ echo "KIND Kubernetes cluster info:" } 2> /dev/null
+kubectl cluster-info --context kind-bridgecrew-workshop
 
-echo ""
-echo "Checkov.io version:"
+{ echo "" } 2> /dev/null
+{ echo "Checkov.io version:" } 2> /dev/null
 checkov --version
 
-echo ""
-echo "Yor.io version:"
+{ echo "" } } 2> /dev/null
+{ echo "Yor.io version:" } 2> /dev/null
 docker run --tty --volume /root:/root bridgecrew/yor --version
 
