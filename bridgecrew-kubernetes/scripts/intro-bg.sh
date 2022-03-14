6039a96a0ea0e6a2871148f136a5a861bfed3884
Matt Johnson <matt@bridgecrew.io>
2022-03-14 23:30:31 +0000

    Move local argo work to katacoda setup scripts

--- bridgecrew-kubernetes/scripts/intro-bg.sh
+++ bridgecrew-kubernetes/scripts/intro-bg.sh
@@ -7,9 +7,25 @@ curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
 chmod +x ./kind
 mv ./kind /usr/bin/kind
 
-echo "Setting up KIND cluster..." | tee > /opt/.signals-intro-bg-status
+echo "Configuring KIND cluster environment..." | tee > /opt/.signals-intro-bg-status
+cd ${WORKSHOP_HOMEDIR}; cat > ./kind-config.yaml << EOF
+apiVersion: kind.x-k8s.io/v1alpha4
+kind: Cluster
+nodes:
+- role: control-plane
+  extraPortMappings:
+  - containerPort: 32080
+    hostPort: 32080
+    listenAddress: "0.0.0.0"
+    protocol: tcp
+  - containerPort: 32443
+    hostPort: 32443
+    listenAddress: "0.0.0.0"
+    protocol: tcp
+EOF
 
-/usr/bin/kind create cluster --name bridgecrew-workshop
+echo "Setting up KIND cluster..." | tee > /opt/.signals-intro-bg-status
+cd ${WORKSHOP_HOMEDIR}; /usr/bin/kind create cluster --name bridgecrew-workshop --config=kind-config.yaml
 
 echo "Installing kubectl cli..." | tee > /opt/.signals-intro-bg-status
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -30,7 +46,27 @@ cd ${WORKSHOP_HOMEDIR}; pipenv install checkov
 echo "Installing Yor..." | tee > /opt/.signals-intro-bg-status
 docker pull bridgecrew/yor
 
-echo "Cloning KustomizeGoat" | tee > /opt/.signals-intro-bg-status
+echo "Cloning KustomizeGoat..." | tee > /opt/.signals-intro-bg-status
 cd ${WORKSHOP_HOMEDIR}; git clone https://github.com/bridgecrewio/kustomizegoat.git
+
+echo "Installing ArgoCD into cluster..." | tee > /opt/.signals-intro-bg-status
+cd ${WORKSHOP_HOMEDIR}; kubectl create namespace argocd
+cd ${WORKSHOP_HOMEDIR}; kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo "Installing ArgoCD CLI..." | tee > /opt/.signals-intro-bg-status
+cd ${WORKSHOP_HOMEDIR}; curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+cd ${WORKSHOP_HOMEDIR}; chmod +x /usr/local/bin/argocd
+
+echo "Setting access to Argo Web UI" | tee > /opt/.signals-intro-bg-status
+kubectl patch svc argocd-server -n argocd --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"},{"op":"replace","path":"/spec/ports/0/nodePort","value":32080},{"op":"replace","path":"/spec/ports/1/nodePort","value":32443}]'
+
+echo "Writing default Argo UI creds to ~/.argo-password" | tee > /opt/.signals-intro-bg-status
+cd ${WORKSHOP_HOMEDIR}; kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > ./.argo-password
+
+
+
+
+
+
 
 echo "done" >> /opt/.signals-intro-bg-finished
--- bridgecrew-kubernetes/scripts/intro-fg.sh
+++ bridgecrew-kubernetes/scripts/intro-fg.sh
@@ -1,20 +1,20 @@
 set +x
 
-{ echo "Waiting for initial workshop setup to complete..."; } 2> /dev/null
+{ echo "Waiting for initial workshop setup to complete..."; } 2>/dev/null
 
 time while [ ! -f /opt/.signals-intro-bg-finished ] ; do tail /opt/.signals-intro-bg-status 2>/dev/null ; sleep 2; done; 
 
-{ echo "Bridgecrew Workshop Environment Ready!" } 2> /dev/null
-{ echo "--------------------------------------" } 2> /dev/null
+{ echo "Bridgecrew Workshop Environment Ready!" } 2>/dev/null
+{ echo "--------------------------------------" } 2>/dev/null
 { echo "" } 2> /dev/null
-{ echo "KIND Kubernetes cluster info:" } 2> /dev/null
+{ echo "KIND Kubernetes cluster info:" } 2>/dev/null
 kubectl cluster-info --context kind-bridgecrew-workshop
 
-{ echo "" } 2> /dev/null
-{ echo "Checkov.io version:" } 2> /dev/null
+{ echo "" } 2>/dev/null
+{ echo "Checkov.io version:" } 2>/dev/null
 checkov --version
 
-{ echo "" } 2> /dev/null
-{ echo "Yor.io version:" } 2> /dev/null
+{ echo "" } 2>/dev/null
+{ echo "Yor.io version:" } 2>/dev/null
 docker run --tty --volume /root:/root bridgecrew/yor --version
 
