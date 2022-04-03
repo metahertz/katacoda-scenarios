2d42453454161f9ab7b59fd0429cb50140a4aa33
Matt Johnson <matt@bridgecrew.io>
2022-04-03 15:37:32 +0100

    automate x509 questions for argo cert

--- aws-bridgecrew-kubernetes/userscripts/personalize.sh
+++ aws-bridgecrew-kubernetes/userscripts/personalize.sh
@@ -15,10 +15,14 @@ echo "Installing latest Checkov Kubernetes admission controller..."
 curl -o ${WORKSHOP_AUTOMATION_DIR}/checkov-admission-controller-setup.sh https://raw.githubusercontent.com/bridgecrewio/checkov/master/admissioncontroller/setup.sh
 chmod +x ${WORKSHOP_AUTOMATION_DIR}/checkov-admission-controller-setup.sh
 sed -i 's/\/usr\/local\/opt\/openssl\/bin\/openssl/openssl/' ${WORKSHOP_AUTOMATION_DIR}/checkov-admission-controller-setup.sh
+sed -i 's/-sha256/-sha256 -subj "\/C=GB\/ST=USSEnterprise\/L=Quadrant42\/O=ExampleCert\/OU=ExampleCert\/CN=bridgecrewworkshop-user.local"' ${WORKSHOP_AUTOMATION_DIR}/checkov-admission-controller-setup.sh
 cd ${WORKSHOP_AUTOMATION_DIR}; ${WORKSHOP_AUTOMATION_DIR}/checkov-admission-controller-setup.sh bc-k8s-ws-cls1 $(cat ${WORKSHOP_AUTOMATION_DIR}/bridgecrewtoken)
 
+echo "Waiting for Argo installation to be complete..."
+until kubectl -n argocd get secret argocd-initial-admin-secret ; do echo "Still waiting on argo..." ; sleep 2 ; done
 
-echo "Logging into to Argo via CLI..." 
+echo "Logging into to Argo via CLI..."
+
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > ${WORKSHOP_AUTOMATION_DIR}/.argo-password
 argocd login 127.0.0.1:32443 --insecure --username admin --password $(cat ${WORKSHOP_AUTOMATION_DIR}/.argo-password)
 
