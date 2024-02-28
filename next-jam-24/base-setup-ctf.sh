13a374787bf62b9534c099adb6dfcaebd948fa70
Matt Johnson <matt+github@metahertz.co.uk>
2024-02-28 01:52:11 +0000

    helps if you merge local changes

--- next-jam-24/base-setup-ctf.sh
+++ next-jam-24/base-setup-ctf.sh
@@ -5,6 +5,8 @@ WORKSHOP_AUTOMATION_DIR=${WORKSHOP_HOMEDIR}/.panworkshop
 
 mkdir -p ${WORKSHOP_AUTOMATION_DIR} || true
 
+apt install -y podman-docker
+
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/bin/kind
