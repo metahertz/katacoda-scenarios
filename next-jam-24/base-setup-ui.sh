7c60a911499c88d0a8c5e6e19bd597eb807f836a
Matt Johnson <matt+github@metahertz.co.uk>
2024-03-12 11:22:13 +0000

    CTF UI Updates

--- next-jam-24/base-setup-ui.sh
+++ next-jam-24/base-setup-ui.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+echo 'APT::Periodic::Update-Package-Lists "0";' > /etc/apt/apt.conf.d/20auto-upgrades
+echo 'APT::Periodic::Unattended-Upgrade "0";' >>  /etc/apt/apt.conf.d/20auto-upgrades
+
 WORKSHOP_USER=ubuntu
 WORKSHOP_HOMEDIR=/home/${WORKSHOP_USER}
 WORKSHOP_AUTOMATION_DIR=${WORKSHOP_HOMEDIR}/.panwctf
@@ -6,16 +9,12 @@ WORKSHOP_AUTOMATION_DIR=${WORKSHOP_HOMEDIR}/.panwctf
 mkdir -p ${WORKSHOP_AUTOMATION_DIR} || true
 sudo apt-get update
 
-
-echo 'APT::Periodic::Update-Package-Lists "0";' > /etc/apt/apt.conf.d/20auto-upgrades
-echo 'APT::Periodic::Unattended-Upgrade "0";' >>  /etc/apt/apt.conf.d/20auto-upgrades
-
+sudo apt install python3
 
 echo "Cloning CTF attack tools..."
 cd ${WORKSHOP_AUTOMATION_DIR}; git clone https://github.com/eurogig/log4sheller.git
 chmod -R 777 ${WORKSHOP_AUTOMATION_DIR}/log4sheller
 cd ${WORKSHOP_AUTOMATION_DIR}/log4sheller ; sudo bash init.sh   
-
 
 
 cd ${WORKSHOP_AUTOMATION_DIR}; curl -fsSL https://code-server.dev/install.sh | sh
